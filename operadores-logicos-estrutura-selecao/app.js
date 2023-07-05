1dc3cd4aef75389b2a8c39a87e1c564db5c1c5d4
Luis Saturnino <luis.hsaturnino@gmail.com>
2023-07-04 22:00:53 -0300

    Exercício de operadores e estrutura de selecao

---
+++ operadores-logicos-estrutura-selecao/app.js
@@ -0,0 +1,36 @@
+const { read } = require('fs');
+
+const readLine = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Esse programa irá checar se você é maior de idade e possui habilitação para entrar no kart.');
+console.log('Além de sua idade, precisamos verificar se você está na lista de presença do horário confirmado.');
+
+readLine.question('Qual o ano do seu nascimento?', ano => {
+    if (ano > 2005){
+        console.log('Você não tem 18 anos.');
+    }else{
+        readLine.question('Você tem habilitação? (Sim/Não)', temHabilitacao => {
+            if(!(temHabilitacao.toUpperCase() === 'SIM')){
+                console.log('Você não tem habilitação para entrar no Kart.');
+
+            }else{
+                readLine.question('Qual o seu nome ?', nome => {
+                    switch(nome){
+                        case 'Luis':
+                            console.log('Bem vindo ao Kart, Luis.');
+                            break;
+                        case 'Henrique':
+                            console.log('Bem vindo ao Kart, Henrique');
+                        default:
+                            console.log('Seu nome não foi identificado na lista de presença');
+                    }
+
+                })
+            }
+        })
+    }
+    
+})
